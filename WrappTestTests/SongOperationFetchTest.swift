146a5423b9aef4ecc5397a110bd69fc272d49f41
NikantVohra <nikant@outlook.com>
2016-05-15 00:21:03 +0530

    adding xctest for song fetching operation

---
+++ WrappTestTests/SongOperationFetchTest.swift
@@ -0,0 +1,39 @@
+//
+//  SongOperationFetchTest.swift
+//  WrappTest
+//
+//  Created by Vohra, Nikant on 5/15/16.
+//  Copyright © 2016 Vohra, Nikant. All rights reserved.
+//
+
+import XCTest
+@testable import WrappTest
+
+class SongOperationFetchTest: XCTestCase {
+
+    let songFetchCount = 47
+    
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testSongFetchOperation() {
+        let songFetchExpectation = expectationWithDescription("songFetchExpectation")
+        var testSongs : [Song] = []
+        SongFetchOperation().fetchSongs { (songs, error) in
+            testSongs = songs
+            songFetchExpectation.fulfill()
+        }
+        self.waitForExpectationsWithTimeout(5) { (error) in
+            XCTAssertEqual(testSongs.count, self.songFetchCount)
+        }
+    }
+
+}
