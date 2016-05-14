9b88a4b9570c0a8a6f83e15f96e48e356be342ad
NikantVohra <nikant@outlook.com>
2016-05-14 18:04:18 +0530

    adding song table view cell for displaying songs in the playlist

---
+++ WrappTest/Views/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  WrappTest
+//
+//  Created by Vohra, Nikant on 5/14/16.
+//  Copyright © 2016 Vohra, Nikant. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var songImageView: UIImageView!
+    
+    
+    func configureCell(song : Song) {
+        songTitleLabel.text = song.title
+        artistNameLabel.text = song.artist
+    }
+    
+}
