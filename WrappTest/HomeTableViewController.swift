82567bb1a42e67fe90ee6568897834afe53a02cb
NikantVohra <nikant@outlook.com>
2016-05-14 17:14:07 +0530

    Initial xcode project commit for a single view application with a HomeTableViewController

---
+++ WrappTest/HomeTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeTableViewController.swift
+//  WrappTest
+//
+//  Created by Vohra, Nikant on 5/14/16.
+//  Copyright © 2016 Vohra, Nikant. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+
+
+
+}
